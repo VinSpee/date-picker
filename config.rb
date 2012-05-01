f22f97d41ed58f75d5eb6c0628a7499b83fbfdb6
Vince Speelman <V@VinSpee.me>
2012-05-01 13:49:09 -0400

    Initialize the template for a new Demo page
    Cloned html5bp
    Removed stuff we don't need (most cross-browser, comments, apple-touch-icon.
    Created favicon.ico

--- config.rb
+++ config.rb
@@ -2,24 +2,18 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/"
-sass_dir = "/"
-images_dir = "/"
-javascripts_dir = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-sass_options = {:debug_info=>true}
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 preferred_syntax = :sass
-
-output_style = :compact # by Fire.app 
-
-sass_options = {:debug_info=>false} # by Fire.app 
-line_comments = false # by Fire.app 
